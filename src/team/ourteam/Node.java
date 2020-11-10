9fe177f9fa7f76f8152cfe4a22978ddd60688126
wanname777 <vannmaureen3@gmail.com>
2020-11-10 22:19:49 +0800

    Initial commit

---
+++ src/team/ourteam/Node.java
@@ -0,0 +1,94 @@
+package team.ourteam;
+
+import java.util.HashMap;
+
+
+/**
+ * @author zhang
+ * @date 2020-11-07 14:38:16
+ */
+public class Node {
+    /**
+     * num表示自己当前的编号，当然再此次实验中我们将Vector的序号与num对应了
+     * status表示当前的状态，由0（未激活）、1（正向传播）、-1（负向传播）组成
+     * influence表示该节点的对外影响力，即出度和
+     * relativeInfluence表示该节点的相对对外影响力
+     * beInfluenced表示该节点的被影响力，即入度和
+     * 两个HashMap分别表示出度表和入度表
+     */
+    int num;
+    int status;
+    double influence;
+    double relativeInfluence;
+    double beInfluenced;
+    HashMap<Integer, Double> outDegree = new HashMap<Integer, Double>();
+    HashMap<Integer, Double> degree = new HashMap<Integer, Double>();
+
+
+    public Node(int num, int status, double influence,
+                double relativeInfluence, double beInfluenced) {
+        this.num = num;
+        this.status = status;
+        this.influence = influence;
+        this.relativeInfluence = relativeInfluence;
+        this.beInfluenced = beInfluenced;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "num=" + num +
+                ", status=" + status +
+                ", influence=" + influence +
+                ", relativeInfluence=" + relativeInfluence +
+                ", beInfluenced=" + beInfluenced +
+                ", outDegree=" + outDegree +
+                ", degree=" + degree +
+                '}';
+    }
+
+    public HashMap<Integer, Double> getOutDegree() {
+        return outDegree;
+    }
+
+    public void setOutDegree(HashMap<Integer, Double> outDegree) {
+        this.outDegree = outDegree;
+    }
+
+    public double getInfluence() {
+        return influence;
+    }
+
+    public void setInfluence(double influence) {
+        this.influence = influence;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void computeInfluence() {
+        for (double d : outDegree.values()) {
+            this.influence += d;
+        }
+    }
+
+    public void computeBeInfluenced() {
+        for (double d : degree.values()) {
+            this.beInfluenced += d;
+        }
+    }
+
+}
---
+++ src/team/ourteam/ReadFiles.java
@@ -0,0 +1,69 @@
+package team.ourteam;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.Vector;
+
+
+/**
+ * @author zhang
+ */
+public class ReadFiles {
+    Vector<Node> nodeVector = new Vector<>();
+
+    public void loadLinks(String path) {
+        int tempNum = 20000;
+        // 初始化vector
+        for (int i = 0; i < tempNum; i++) {
+            nodeVector.add(new Node(i, 0, 0., 0., 0.));
+        }
+
+        LineNumberReader reader = null;
+        FileReader in = null;
+        String[] pairs;
+
+        try {
+            in = new FileReader(path);
+            reader = new LineNumberReader(in);
+
+            while (true) {
+                String str = reader.readLine();
+                if (str == null) {
+                    break;
+                }
+                pairs = str.split(" ");
+
+                int num1 = Integer.parseInt(pairs[0]);
+                int num2 = Integer.parseInt(pairs[1]);
+                double d1 = Double.parseDouble(pairs[2]);
+                double d2 = Double.parseDouble(pairs[3]);
+
+                // 计算当前点的出度和被指向点的入度
+                nodeVector.get(num1).outDegree.put(num2, d1 * d2);
+                nodeVector.get(num2).degree.put(num1, d1 * d2);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
